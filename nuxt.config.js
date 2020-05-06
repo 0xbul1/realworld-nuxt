0093eacd2533c66800e20182f4e30c77bfc96d23
0xbul1 <diaoszhao@gmail.com>
2020-05-06 12:21:11 +0800

    完成上线部署spa模式

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,12 @@
 export default {
+  mode: 'spa',
   // plugins: ['~/plugins/axios-interceptors.js'],
   plugins: ['~/plugins/request.js'],
+  // publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  build: {
+    // 访问路径是怎样的就怎么配置
+    // publicPath: 'http://03em02.coding-pages.com/',
+    publicPath: 'http://nuxt.20201060.xyz/',
+    // publicPath: 'http://127.0.0.1:5500/dist/',
+  },
 };
